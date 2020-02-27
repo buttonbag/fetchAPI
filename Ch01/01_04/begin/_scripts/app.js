a3eded5ad12895a6f868b0359e43f3445ea9b430
Louie.Camacho <louie.camacho@uschcamaloalx15.omega.wwads.net>
2020-02-27 15:42:31 -0600

    added APIs

--- Ch01/01_04/begin/_scripts/app.js
+++ Ch01/01_04/begin/_scripts/app.js
@@ -1,2 +1,38 @@
 'use strict';
 
+const url = 'https://us-street.api.smartystreets.com/street-address?auth-id=3377004542077807&candidates=10&match=invalid&street=86%20Frontage%20Road&street2=&city=Belmont&state=MA';
+const nps = 'https://developer.nps.gov/api/v1/parks?parkCode=acad&api_key=9SMjAXxMbVpMYIpG6lEHNRVbh9haR7GHbzfUItEu';
+
+const updateUISuccess = function(data) {
+    console.log(data);
+};
+const updateUIError = function(data) {
+    console.log(data);
+};
+
+const responseMethod = function(httpRequest) {
+    if (httpRequest.readyState === 4) {
+        if (httpRequest.status === 200) {
+            updateUISuccess(httpRequest.responseText);
+        } else {
+            updateUIError(httpRequest.status + ': ' + httpRequest.responseText);
+        }
+    }
+}
+
+const createRequest = function(url) {
+    const httpRequest = new XMLHttpRequest(url);
+    httpRequest.addEventListener('readystatechange', (url) => responseMethod(httpRequest))
+    httpRequest.open('GET', url);
+    httpRequest.send();
+};
+
+const createRequestNPS = function(nps) {
+    const httpRequest = new XMLHttpRequest(nps);
+    httpRequest.addEventListener('readystatechange', (nps) => responseMethod(httpRequest))
+    httpRequest.open('GET', nps);
+    httpRequest.send();
+};
+
+createRequest(url);
+createRequestNPS(nps);
