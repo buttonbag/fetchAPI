336eca8502289f97b3510bc31e1cc0effbb4f0c7
Louie.Camacho <louie.camacho@uschcamaloalx15.omega.wwads.net>
2020-02-27 17:18:27 -0600

    checkCompletion function set to request 3 params

--- Ch01/01_04/begin/_scripts/app.js
+++ Ch01/01_04/begin/_scripts/app.js
@@ -1,7 +1,13 @@
 'use strict';
 
-const smartyUrl = 'https://us-street.api.smartystreets.com/street-address?auth-id=3377004542077807&candidates=10&match=invalid&street=86%20Frontage%20Road&street2=&city=Belmont&state=MA';
+// const smartyUrl = 'https://us-street.api.smartystreets.com/street-address?auth-id=3377004542077807&candidates=10&match=invalid&street=86%20Frontage%20Road&street2=&city=Belmont&state=MA';
+const smartyUrl = 'https://us-street.api.smartystreets.com/street-address?auth-id=3377004542077807&candidates=10';
 const nps = 'https://developer.nps.gov/api/v1/parks?stateCode=ca&api_key=9SMjAXxMbVpMYIpG6lEHNRVbh9haR7GHbzfUItEu';
+const addressField = document.querySelector('#address');
+const cityField = document.querySelector('#city');
+const stateField = document.querySelector('#state');
+const zipField = document.querySelector('#zip');
+
 
 const updateUISuccess = function(data) {
     console.log(data);
@@ -27,5 +33,21 @@ const createRequest = function(url) {
     httpRequest.send();
 };
 
-createRequest(smartyUrl);
+const checkCompletion = function () {
+    if (addressField.value !== '' &&
+        cityField.value !== '' &&
+        stateField.value !== '') {
+            const requestUrl = smartyUrl +
+                '&street=' + addressField.value +
+                '&city=' + cityField.value +
+                '&state=' + stateField.value;
+            createRequest(requestUrl);
+    }
+}
+
+// createRequest(smartyUrl);
 createRequest(nps);
+
+addressField.addEventListener('blur', checkCompletion);
+cityField.addEventListener('blur', checkCompletion);
+stateField.addEventListener('blur', checkCompletion);
