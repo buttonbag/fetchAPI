b7b6783a2a38f6e962dc7aae363419196e029cbd
Louie Camacho <louie.camacho@hogarth-ogilvy.com>
2020-03-16 15:11:00 -0500

    converting to fetch api instead

--- Ch01/01_04/begin/_scripts/app.js
+++ Ch01/01_04/begin/_scripts/app.js
@@ -2,6 +2,12 @@
 
 // const smartyUrl = 'https://us-street.api.smartystreets.com/street-address?auth-id=3377004542077807&candidates=10&match=invalid&street=86%20Frontage%20Road&street2=&city=Belmont&state=MA';
 const smartyUrl = 'https://us-street.api.smartystreets.com/street-address?auth-id=3377004542077807&candidates=10';
+const smartyInit = {
+    headers: {
+        'Content-Type': 'application/json',
+        Host: 'us-street.api.smartystreets.com',
+    },
+};
 const nps = 'https://developer.nps.gov/api/v1/parks?stateCode=ca&api_key=9SMjAXxMbVpMYIpG6lEHNRVbh9haR7GHbzfUItEu';
 const addressField = document.querySelector('#address');
 const cityField = document.querySelector('#city');
@@ -14,23 +20,23 @@ const parkDesc = document.querySelector('#specials p');
 
 
 
-const smartyUpdateUISuccess = function(data) {
-    const parseData = JSON.parse(data);
-    // console.log(parseData);
-    const zip = parseData[0].components.zipcode;
-    const plus4 = parseData[0].components.plus4_code;
+const smartyUpdateUISuccess = function(parsedData) {
+    // const parsedData = JSON.parse(data);
+    // console.log(parsedData);
+    const zip = parsedData[0].components.zipcode;
+    const plus4 = parsedData[0].components.plus4_code;
     zipField.value = (zip + '-' + plus4);
 };
 const smartyUpdateUIError = function(error) {
     console.log(error);
 };
-const npsUpdateUISuccess = function(data) {
-    const parseData = JSON.parse(data);
-    console.log(parseData);
-    const number = Math.floor(Math.random() * parseData.data.length);
-    parkName.textContent = parseData.data[number].fullName;
-    parkName.href = parseData.data[number].url;
-    parkDesc.textContent = parseData.data[number].description;
+const npsUpdateUISuccess = function(parsedData) {
+    // const parsedData = JSON.parse(data);
+    console.log(parsedData);
+    const number = Math.floor(Math.random() * parsedData.data.length);
+    parkName.textContent = parsedData.data[number].fullName;
+    parkName.href = parsedData.data[number].url;
+    parkDesc.textContent = parsedData.data[number].description;
     parkThumb.src = 'https://www.nps.gov/common/commonspot/templates/assetsCT/images/branding/logo.png';
     parkSection.classList.remove('hidden');
 };
@@ -38,21 +44,35 @@ const npsUpdateUIError = function(error) {
     console.log(error);
 };
 
-const responseMethod = function(httpRequest, succeed, fail) {
-    if (httpRequest.readyState === 4) {
-        if (httpRequest.status === 200) {
-            succeed(httpRequest.responseText);
-        } else {
-            fail(httpRequest.status + ': ' + httpRequest.responseText);
-        }
+// const responseMethod = function(httpRequest, succeed, fail) {
+//     if (httpRequest.readyState === 4) {
+//         if (httpRequest.status === 200) {
+//             succeed(httpRequest.responseText);
+//         } else {
+//             fail(httpRequest.status + ': ' + httpRequest.responseText);
+//         }
+//     }
+// }
+
+// const createRequest = function(url, succeed, fail) {
+//     const httpRequest = new XMLHttpRequest(url);
+//     httpRequest.addEventListener('readystatechange', (url) => responseMethod(httpRequest, succeed, fail))
+//     httpRequest.open('GET', url);
+//     httpRequest.send();
+// };
+
+const handleErrors = function(response) {
+    if(!response.ok) {
+        throw (response.status + ': ' + response.statusText);
     }
+    return response.json();
 }
 
-const createRequest = function(url, succeed, fail) {
-    const httpRequest = new XMLHttpRequest(url);
-    httpRequest.addEventListener('readystatechange', (url) => responseMethod(httpRequest, succeed, fail))
-    httpRequest.open('GET', url);
-    httpRequest.send();
+const createRequest = function(url, succeed, fail, init) {
+    fetch(url, init)
+        .then((response) => handleErrors(response))
+        .then((data) => succeed(data))
+        .catch((error) => fail(error));
 };
 
 const checkCompletion = function () {
@@ -63,7 +83,7 @@ const checkCompletion = function () {
                 '&street=' + addressField.value +
                 '&city=' + cityField.value +
                 '&state=' + stateField.value;
-            createRequest(requestUrl, smartyUpdateUISuccess, smartyUpdateUIError);
+            createRequest(requestUrl, smartyUpdateUISuccess, smartyUpdateUIError, smartyInit);
     }
 }
 
